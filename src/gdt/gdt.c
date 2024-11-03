9303418952fd460b2733b3bf371e0fc720e6c27d
hasanpasha <bashahsn22@gmail.com>
2024-11-03 23:20:32 +0300

    refactor

--- src/gdt/gdt.c
+++ src/gdt/gdt.c
@@ -11,25 +11,34 @@ void init_gdt() {
     gdt_p.size = (sizeof(struct gdt_entry_struct) * GDT_TABLE_SIZE) - 1;
     gdt_p.base = (uint32_t)&gdt_entries;
 
-    struct gdt_entry_flags_struct null_flags = { .G = 0, .DB = 0, .L = 0 };
-    struct gdt_entry_flags_struct other_flags = { .G = 1, .DB = 1, .L = 0 };
+    union gdt_flags_union null_flags = { .flags_value = 0 };
+    union gdt_access_union null_access = { .access_value = 0 };
+    set_gdt_gate(0, 0, 0, null_access, null_flags); // NULL segment
 
-    set_gdt_gate(0, 0, 0, 0, null_flags); // NULL segment
-    set_gdt_gate(1, 0, 0xFFFFF, 0x9A, other_flags); // kernel code segment
-    set_gdt_gate(2, 0, 0xFFFFF, 0x92, other_flags); // kernel data segment
-    set_gdt_gate(3, 0, 0xFFFFF, 0xFA, other_flags); // User code segment
-    set_gdt_gate(4, 0, 0xFFFFF, 0xF2, other_flags); // User data segment
+    union gdt_flags_union other_flags = { .flags = { .G = 1, .DB = 1, .L = 0 } };   // 0b1100, 0xC
+
+    union gdt_access_union kernel_code_access = { .access = { .P = 1, .DPL = 0, .S = 1, .E = 1, .DC = 0, .RW = 1, .A = 0 } }; // 0b10011010 , 0x9A
+    set_gdt_gate(1, 0, 0xFFFFF, kernel_code_access, other_flags); // kernel code segment
+
+    union gdt_access_union kernel_data_access = { .access = { .P = 1, .DPL = 0, .S = 1, .E = 0, .DC = 0, .RW = 1, .A = 0 } }; // 0b10010010, 0x92
+    set_gdt_gate(2, 0, 0xFFFFF, kernel_data_access, other_flags); // kernel data segment
+
+    union gdt_access_union user_code_access = { .access = { .P = 1, .DPL = 3, .S = 1, .E = 1, .DC = 0, .RW = 1, .A = 0 } }; // 0b11111010, 0xFA
+    set_gdt_gate(3, 0, 0xFFFFF, user_code_access, other_flags); // User code segment
+
+    union gdt_access_union user_data_access = { .access = { .P = 1, .DPL = 3, .S = 1, .E = 0, .DC = 0, .RW = 1, .A = 0 } }; // 0b11110010, 0xF2
+    set_gdt_gate(4, 0, 0xFFFFF, user_data_access, other_flags); // User data segment
 
     gdt_flush((uint32_t)&gdt_p);
 }
 
-void set_gdt_gate(uint32_t num, uint32_t base, uint32_t limit, uint8_t access, gdt_entry_flags flags) {
+void set_gdt_gate(uint32_t num, uint32_t base, uint32_t limit, union gdt_access_union access, union gdt_flags_union flags) {
     gdt_entries[num].base_low = (base & 0xFFFF);
     gdt_entries[num].base_middle = (base >> 16) & 0xFF;
     gdt_entries[num].base_high = (base >> 24) & 0xFF;
 
     gdt_entries[num].limit_low = (limit & 0xFFFF);
     gdt_entries[num].limit_high = (limit >> 16) & 0x0F;
-    gdt_entries[num].access = access;
-    gdt_entries[num].flags = flags;
+    gdt_entries[num].access = access.access_value;
+    gdt_entries[num].flags = flags.flags_value;
 }
--- src/gdt/gdt.h
+++ src/gdt/gdt.h
@@ -3,11 +3,34 @@
 
 struct gdt_entry_flags_struct {
     uint8_t pad : 1;
-    uint8_t L : 1;
-    uint8_t DB : 1;
-    uint8_t G : 1;
+    uint8_t L : 1;  // Long-mode code flag
+    uint8_t DB : 1; // Size flag
+    uint8_t G : 1;  // Granularity flag
 } __attribute__((packed));
 typedef struct gdt_entry_flags_struct gdt_entry_flags;
+
+union gdt_flags_union
+{
+    uint8_t flags_value : 4;
+    struct gdt_entry_flags_struct flags;
+} __attribute__((packed));
+
+struct gdt_entry_access_struct {
+    uint8_t A: 1;   // Accessed bit
+    uint8_t RW: 1;  // Readable bit/Writable bit
+    uint8_t DC: 1;  // Direction bit/Conforming bit
+    uint8_t E: 1;   // Executable bit
+    uint8_t S: 1;   // Descriptor type bit
+    uint8_t DPL: 2; // Descriptor privilege level field
+    uint8_t P: 1;   // Present bit
+} __attribute__((packed));
+typedef struct gdt_entry_access_struct gdt_entry_access;
+
+union gdt_access_union
+{
+    uint8_t access_value;
+    struct gdt_entry_access_struct access;
+} __attribute__((packed));
 
 struct gdt_entry_struct
 {
@@ -16,7 +39,8 @@ struct gdt_entry_struct
     uint8_t base_middle;
     uint8_t access;
     uint8_t limit_high : 4;
-    struct gdt_entry_flags_struct flags;
+    uint8_t flags : 4;
+    // gdt_flags flags;
     uint8_t base_high;
 } __attribute__((packed));
 typedef struct gdt_entry_struct gdt_entry;
@@ -28,4 +52,4 @@ struct gdt_ptr_struct {
 typedef struct gdt_ptr_struct gdt_ptr;
 
 void init_gdt();
-void set_gdt_gate(uint32_t num, uint32_t base, uint32_t limit, uint8_t access, gdt_entry_flags flags);
+void set_gdt_gate(uint32_t num, uint32_t base, uint32_t limit, union gdt_access_union access, union gdt_flags_union flags);
