7ae0114ab75e2a38b7c34d1e00d917c5d58d8aea
hasanpasha <bashahsn22@gmail.com>
2024-11-03 22:18:11 +0300

    use flags struct

--- src/gdt/gdt.c
+++ src/gdt/gdt.c
@@ -4,29 +4,32 @@ extern void gdt_flush(uint32_t);
 
 #define GDT_TABLE_SIZE 5
 
-struct gdt_entry_struct gdt_entries[GDT_TABLE_SIZE];
-struct gdt_ptr_struct gdt_ptr;
+gdt_entry gdt_entries[GDT_TABLE_SIZE];
+gdt_ptr gdt_p;
 
 void init_gdt() {
-    gdt_ptr.size = (sizeof(struct gdt_entry_struct) * GDT_TABLE_SIZE) - 1;
-    gdt_ptr.base = (uint32_t)&gdt_entries;
+    gdt_p.size = (sizeof(struct gdt_entry_struct) * GDT_TABLE_SIZE) - 1;
+    gdt_p.base = (uint32_t)&gdt_entries;
 
-    set_gdt_gate(0, 0, 0, 0, 0); // NULL segment
-    set_gdt_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); // kernel code segment
-    set_gdt_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF); // kernel data segment
-    set_gdt_gate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // User code segment
-    set_gdt_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // User data segment
+    struct gdt_entry_flags_struct null_flags = { .G = 0, .DB = 0, .L = 0 };
+    struct gdt_entry_flags_struct other_flags = { .G = 1, .DB = 1, .L = 0 };
 
-    gdt_flush((uint32_t)&gdt_ptr);
+    set_gdt_gate(0, 0, 0, 0, null_flags); // NULL segment
+    set_gdt_gate(1, 0, 0xFFFFF, 0x9A, other_flags); // kernel code segment
+    set_gdt_gate(2, 0, 0xFFFFF, 0x92, other_flags); // kernel data segment
+    set_gdt_gate(3, 0, 0xFFFFF, 0xFA, other_flags); // User code segment
+    set_gdt_gate(4, 0, 0xFFFFF, 0xF2, other_flags); // User data segment
+
+    gdt_flush((uint32_t)&gdt_p);
 }
 
-void set_gdt_gate(uint32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran) {
+void set_gdt_gate(uint32_t num, uint32_t base, uint32_t limit, uint8_t access, gdt_entry_flags flags) {
     gdt_entries[num].base_low = (base & 0xFFFF);
     gdt_entries[num].base_middle = (base >> 16) & 0xFF;
     gdt_entries[num].base_high = (base >> 24) & 0xFF;
 
     gdt_entries[num].limit_low = (limit & 0xFFFF);
     gdt_entries[num].limit_high = (limit >> 16) & 0x0F;
-    gdt_entries[num].flags = (gran & 0x0F);
     gdt_entries[num].access = access;
+    gdt_entries[num].flags = flags;
 }
--- src/gdt/gdt.h
+++ src/gdt/gdt.h
@@ -1,5 +1,14 @@
 #include "../include/stdint.h"
 
+
+struct gdt_entry_flags_struct {
+    uint8_t pad : 1;
+    uint8_t L : 1;
+    uint8_t DB : 1;
+    uint8_t G : 1;
+} __attribute__((packed));
+typedef struct gdt_entry_flags_struct gdt_entry_flags;
+
 struct gdt_entry_struct
 {
     uint16_t limit_low;
@@ -7,14 +16,16 @@ struct gdt_entry_struct
     uint8_t base_middle;
     uint8_t access;
     uint8_t limit_high : 4;
-    uint8_t flags : 4;
+    struct gdt_entry_flags_struct flags;
     uint8_t base_high;
 } __attribute__((packed));
+typedef struct gdt_entry_struct gdt_entry;
 
 struct gdt_ptr_struct {
     uint16_t size;
     unsigned int base;
 }__attribute__((packed));
+typedef struct gdt_ptr_struct gdt_ptr;
 
 void init_gdt();
-void set_gdt_gate(uint32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran);
+void set_gdt_gate(uint32_t num, uint32_t base, uint32_t limit, uint8_t access, gdt_entry_flags flags);
